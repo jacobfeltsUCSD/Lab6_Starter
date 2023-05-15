b4c556c56bb6949518ba7aa3ab160c1f2419d04b
Jacob Felts <jfelts@ucsd.edu>
2023-05-14 22:54:55 -0700

    small edit to hopefully fix github page

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -58,10 +58,10 @@ class RecipeCard extends HTMLElement {
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
     articleRec.innerHTML = `
-    <img src="${data.imgSrc}"
-      alt="${data.imgAlt}">
+    <img src=${data.imgSrc}
+      alt=${data.imgAlt}>
     <p class="title">
-      <a href="${data.titleLnk}">${data.titleTxt}</a>
+      <a href=${data.titleLnk}>${data.titleTxt}</a>
     </p>
     <p class="organization">${organization}</p>
     <div class="rating">
